5cf4b0bee4b4216c8f42461349007f98bdc1bcea
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2022-05-01 14:00:13 +0300

    ...

--- hackerrank/src/tr/com/ahmet/FlippingMatrix.java
+++ hackerrank/src/tr/com/ahmet/FlippingMatrix.java
@@ -11,10 +11,10 @@ public class FlippingMatrix {
 //        matrix.add(new ArrayList<>(Arrays.asList(1, 4)));
 //        matrix.add(new ArrayList<>(Arrays.asList(3, 2)));
 
-        matrix.add(new ArrayList<>(Arrays.asList(112, 42, 83, 119)));
-        matrix.add(new ArrayList<>(Arrays.asList(56, 125, 56, 49)));
-        matrix.add(new ArrayList<>(Arrays.asList(15, 78, 101, 43)));
-        matrix.add(new ArrayList<>(Arrays.asList(62, 98, 114, 108)));
+        matrix.add(new ArrayList<>(Arrays.asList(4, 7, 15, 13)));
+        matrix.add(new ArrayList<>(Arrays.asList(8, 4, 14, 5)));
+        matrix.add(new ArrayList<>(Arrays.asList(11, 3, 5, 11)));
+        matrix.add(new ArrayList<>(Arrays.asList(16, 2, 9, 10)));
 
         System.out.println(flippingMatrix(matrix));
     }
@@ -26,7 +26,7 @@ public class FlippingMatrix {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 maxSum += Math.max(Math.max(matrix.get(i).get(j), matrix.get(i).get(2*n - j - 1)),
-                                   Math.max(matrix.get(2*n - i - 1).get(j), matrix.get(2*n - i - 1).get(2 * n - j - 1)));
+                                   Math.max(matrix.get(2*n - i - 1).get(j), matrix.get(2*n - i - 1).get(2*n - j - 1)));
             }
         }
         return maxSum;
