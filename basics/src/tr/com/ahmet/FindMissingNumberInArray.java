e2ff963f8d42a5f68c8230e513c4aa1ab4a82f70
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-03-14 21:13:49 +0300

    ...

---
+++ basics/src/tr/com/ahmet/FindMissingNumberInArray.java
@@ -0,0 +1,22 @@
+package tr.com.ahmet;
+
+public class FindMissingNumberInArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 4, 8, 5, 6, 7};
+        System.out.println(new FindMissingNumberInArray().missingNum(arr));
+
+        int[] arr2 = {3, 4, 2, 5};
+        System.out.println(new FindMissingNumberInArray().missingNum(arr2));
+    }
+
+    private int missingNum(int[] arr) {
+        int l = arr.length + 1;
+        int sum = l * (l + 1) / 2;
+        for (int e: arr) {
+            sum -= e;
+        }
+        return sum;
+    }
+}
