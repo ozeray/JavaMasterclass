7928f53383aae01e8278c7d88a827fabd1bb2f86
Ahmet Ozer <ahmet.ozer@scalefocus.com>
2023-11-04 18:17:42 +0300

    ...

---
+++ basics/src/com/ahmet/completejavadevelopercourse/collections/Main.java
@@ -0,0 +1,11 @@
+package com.ahmet.completejavadevelopercourse.collections;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
