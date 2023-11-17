18a69ecbef9dd538fe54e6a3a77eddcb1218def4
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-11-17 18:35:44 +0300

    ...

---
+++ basics/src/com/ahmet/completejavadeveloper/math/MathDemo.java
@@ -0,0 +1,24 @@
+package com.ahmet.completejavadeveloper.math;
+
+import java.util.stream.IntStream;
+
+public class MathDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+        System.out.println(Math.abs(-5));
+        System.out.println(Math.abs(-2147483647));
+
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.random());
+
+        System.out.println((char) 65);
+        System.out.println((char) (65 + 25));
+        System.out.println((char) 97);
+        System.out.println((char) (97 + 25));
+        System.out.println((char) (Math.random() * 26 + 65)); // Random char A-Z
+        System.out.println((char) (Math.random() * 26 + 97)); // Random char a-z
+    }
+}
