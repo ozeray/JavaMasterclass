43c071e7657a2049510aeed1e1386d142660d122
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-11-27 00:06:48 +0300

    ...

---
+++ basics/src/com/ahmet/completejavadeveloper/io/ReadingFiles.java
@@ -0,0 +1,31 @@
+package com.ahmet.completejavadeveloper.io;
+
+import java.io.*;
+
+public class ReadingFiles {
+    public static void main(String[] args) {
+//        try(FileReader reader = new FileReader("basics/files/file.txt")) {
+        try(FileReader reader = new FileReader("basics/files/file.txt")) {
+            char[] block = new char[1000];
+            int data;
+            while ((data = reader.read(block)) != -1) {
+                String content = new String(block, 0, data);
+                System.out.printf("[%d chars] %s%n", data, content);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("----------------------------------------------------------------");
+        try(BufferedReader br = new BufferedReader(new FileReader("basics/files/file.txt"))) {
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+            // Easier, after JDK 1.8:
+            br.lines().forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
