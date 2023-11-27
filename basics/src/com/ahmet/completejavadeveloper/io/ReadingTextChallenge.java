6523e01accf3012af4b0e9814edea0d05e84bd6f
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-11-27 16:01:17 +0300

    ...

---
+++ basics/src/com/ahmet/completejavadeveloper/io/ReadingTextChallenge.java
@@ -0,0 +1,71 @@
+package com.ahmet.completejavadeveloper.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ReadingTextChallenge {
+
+    public static void main(String[] args) {
+//        Path path = Path.of("basics/files/ai.txt");
+        Path path = Path.of("basics/files/pytest.txt");
+        printTokens(path);
+        printTokensWithoutPunctuation(path);
+        printTokensWithoutPunctuationLongThanFiveChars(path);
+        printTopTenTokens(path);
+    }
+
+    private static void printTokens(Path path) {
+        try {
+            Scanner scanner = new Scanner(path);
+            scanner.tokens().forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printTokensWithoutPunctuation(Path path) {
+        try {
+            System.out.println("----------------------------");
+            String[] tokens = Files.readString(path)
+                    .replaceAll("\\p{Punct}", "")
+                    .split("\\p{javaWhitespace}");
+            System.out.println(Arrays.toString(tokens));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printTokensWithoutPunctuationLongThanFiveChars(Path path) {
+        try {
+            System.out.println("----------------------------");
+            Scanner scanner = new Scanner(path);
+            scanner.findAll("\\w{6,}\\p{javaWhitespace}")
+                    .forEach(m -> System.out.println(m.group()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printTopTenTokens(Path path) {
+        try {
+            System.out.println("------------------------------");
+            Scanner scanner = new Scanner(path);
+            scanner.tokens()
+                    .map(token -> token.replaceAll("\\p{Punct}", ""))
+                    .filter(s -> s.length() > 5)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .entrySet()
+                    .stream().sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                    .limit(10)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
