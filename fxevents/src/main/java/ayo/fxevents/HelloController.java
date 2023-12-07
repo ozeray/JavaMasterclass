37a6742a901f05e64bb8af1ee7a27c4f752cf8bb
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-12-07 21:37:04 +0300

    ...

--- fxevents/src/main/java/ayo/fxevents/HelloController.java
+++ fxevents/src/main/java/ayo/fxevents/HelloController.java
@@ -2,7 +2,6 @@ package ayo.fxevents;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
